5d14d39ea9031cdf905191a349c7961fe84b243a
Vincent <vinkang97@gmail.com>
2016-07-21 19:44:47 +0000

    Server done

--- server.js
+++ server.js
@@ -1,11 +1,42 @@
 var express = require('express')
-
+var path = require('path')
+var fs = require('fs')
+var moment = require('moment');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    var fileName = path.join(__dirname,'index.html');
+    res.sendFile(fileName, function(err){
+        if (err){
+            console.log(err)
+            res.status(err.status).end();
+        }else{
+            console.log('Sent:',fileName);
+        }
+    })
 });
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+  console.log('App listening on port 8080!');
 });
+
+app.get('/:datestring', function(req, res){
+    var myDate;
+    if(/^\d{8,}$/.test(req.params.datestring)) {
+        myDate = moment(req.params.datestring, "X");
+    } else {
+        myDate = moment(req.params.datestring, "MMMM D, YYYY");
+    }
+    
+    if (myDate.isValid()){
+        res.json({
+          unix: myDate.format("X"),
+          natural: myDate.format("MMMM D, YYYY")
+        });
+    }else{
+        res.json({
+            unix:null,
+            natural: null
+        })
+    }
+})
